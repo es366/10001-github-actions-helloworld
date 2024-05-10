380f915b5b215c66813057c2c7af597e79f7c999
gnuhub <gnuhub@qq.com>
2024-05-10 15:11:28 +0800

    [just fix it]

--- 1.ci.sh
+++ 1.ci.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+
 echo "hello world"
 cd ~
 git clone --depth 1 https://github.com/elastic/elasticsearch.git 
@@ -11,4 +16,5 @@ cd elasticsearch
 ls -al build/distribution/local/
 cd build/distribution/local/
 tar cvf elasticsearch-8.15.0-SNAPSHOT.tar elasticsearch-8.15.0-SNAPSHOT
+cd $CMD_PATH
 ls -al build/distribution/local/
