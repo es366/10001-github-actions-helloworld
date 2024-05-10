39019e124f98e5642843b85ca9e0cf9969eb305d
gnuhub <gnuhub@qq.com>
2024-05-10 13:53:19 +0800

    [just fix it]

--- 1.ci.sh
+++ 1.ci.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 set -x
 echo "hello world"
+cd ~
+git clone --depth 1 https://github.com/elastic/elasticsearch.git 
+cd elasticsearch
+./gradlew localDistro
+./gradlew :distribution:archives:linux-tar:assembl
