d722ed6e5231ac0a0d0864b889e8b924f9fb7b21
Zeri8 <156058538+Zeri8@users.noreply.github.com>
2024-05-11 10:43:45 +0800

    Update 1.ci.sh

--- 1.ci.sh
+++ 1.ci.sh
@@ -7,18 +7,20 @@ cd $CMD_PATH
 
 echo "hello world"
 cd ~
-git clone -b dev_240510_01 --depth 1 https://github.com/es366/10003-data-elasticsearch.git elasticsearch
+git clone -b dev_240511_01 --depth 1 https://github.com/es366/10003-data-elasticsearch.git elasticsearch
 cd elasticsearch
 ./gradlew localDistro
 ./gradlew :distribution:archives:linux-tar:assembl
 
 # tree -L 3
-ls -al build/distribution/local/elasticsearch-8.15.0-SNAPSHOT
+ls -al build/distribution/local/
+
 cd build/distribution/local/
-tar cf elasticsearch-8.15.0-SNAPSHOT.tar elasticsearch-8.15.0-SNAPSHOT
-ls -al  
-file elasticsearch-8.15.0-SNAPSHOT.tar
-cp elasticsearch-8.15.0-SNAPSHOT.tar ~/
-cd ~/elasticsearch
-file build/distribution/local/elasticsearch-8.15.0-SNAPSHOT.tar
-file ~/elasticsearch-8.15.0-SNAPSHOT.tar
+
+# tar cf elasticsearch-8.15.0-SNAPSHOT.tar elasticsearch-8.15.0-SNAPSHOT
+# ls -al  
+# file elasticsearch-8.15.0-SNAPSHOT.tar
+# cp elasticsearch-8.15.0-SNAPSHOT.tar ~/
+# cd ~/elasticsearch
+# file build/distribution/local/elasticsearch-8.15.0-SNAPSHOT.tar
+# file ~/elasticsearch-8.15.0-SNAPSHOT.tar
